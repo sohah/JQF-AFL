c837f6b0fda2d9fbd05a1675d86438b161b82d70
sohah <husseinsoh@gmail.com>
2025-06-18 12:15:55 +0300

    adding some commands for running the script

--- afl-sv-comp/scripts/run-sv-comp.sh
+++ afl-sv-comp/scripts/run-sv-comp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-#run this script as
+#run this script as the following from the root of jqf, i.e., jqf-afl-paper
+#./afl-sv-comp/scripts/run-sv-comp.sh
+# assumes a directory structure of
+#    bench-defs -> something(git) ->jqf->afl
+#                                 ->afl-sv-comp->scripts
+#                                              ->sv-comp-false.txt
+#
 
 DURATION=800
 
@@ -13,3 +19,7 @@ while read -r line; do
 
   timeout "$DURATION"s ./afl-sv-comp/scripts/alf-svcomp.sh --64 --propertyfile $PWD/../../bench-defs/sv-benchmarks/java/properties/assert_java.prp  $PWD/../../bench-defs/sv-benchmarks/java/common/ $PWD/../../bench-defs/sv-benchmarks/java/"$dirname"/"$benchmark"
 done < "$unsafe_tasks"
+
+
+# to reproduce run a command similar to
+# ./bin/jqf-repro -c .:./afl-sv-comp/:/tmp/jpf-benchmark.fP6ldG/target/classes MainTest mainTest fuzz-results/queue/id\:000000\,orig\:rand100.bin
