99b7c38be7d1f47cacac0bcbbed5f43e9ebefba6
sohah <husseinsoh@gmail.com>
2025-08-17 13:26:20 +0300

    updating write-tests-main script to include the entire directory before the yml file

--- afl-sv-comp/scripts/write-tests-main.sh
+++ afl-sv-comp/scripts/write-tests-main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #run this script as the following from the root of jqf, i.e., jqf-afl-paper
-#./afl-sv-comp/scripts/run-sv-comp.sh
+#./afl-sv-comp/scripts/write-tests-main.sh
 # assumes a directory structure of
 #    bench-defs -> something(git) ->jqf->afl
 #                                 ->afl-sv-comp->scripts
@@ -14,7 +14,9 @@ unsafe_tasks=$PWD/afl-sv-comp/sv-comp-false.txt;
 while IFS= read -r line || [[ -n "$line" ]]; do
   benchmark_yml="${line##*/}"        # Get 'coral22.yml'
   benchmark="${benchmark_yml%.yml}"       # Remove '.yml'
-  dirname=$(echo "$line" | cut -d'/' -f1)
+  echo line=$line
+  dirname=$(dirname "$line")
+  echo "dirname1=$dirname"
   FIND_OPTIONS="-name 'Main.java'"
   source_path="$PWD/../../bench-defs/sv-benchmarks/java/$dirname/$benchmark"
   files=(`eval "find $source_path $FIND_OPTIONS"`)
@@ -35,5 +37,26 @@ while IFS= read -r line || [[ -n "$line" ]]; do
 done < "$unsafe_tasks"
 
 
-# to reproduce run a command similar to
-# ./bin/jqf-repro -c .:./afl-sv-comp/:/tmp/jpf-benchmark.fP6ldG/target/classes MainTest mainTest fuzz-results/queue/id\:000000\,orig\:rand100.bin
+#Note that for some of the java-ranger verification tasks they do not have the same name pattern as in the remaining of the benchmarks, and thus we have changed their names manually. More precisely, we have ran this script on the sv-comp-false.txt where it has the following names for some of java-ranger benchmarks:
+#
+#java-ranger-regression/nanoxml_eqchk/nanoxml_prop2.yml
+#java-ranger-regression/nanoxml_eqchk/nanoxml_prop3.yml
+#java-ranger-regression/printtokens_eqchk/printtokens_prop2.yml
+#java-ranger-regression/replace5_eqchk/replace5_prop2.yml
+#java-ranger-regression/siena_eqchk/siena_prop2.yml
+#java-ranger-regression/WBS/WBS_prop1.yml
+#java-ranger-regression/WBS/WBS_prop3.yml
+#java-ranger-regression/WBS/WBS_prop4.yml
+#
+#instead of having these names
+#
+#java-ranger-regression/nanoxml_eqchk/nanoxml_prop2.yml
+#java-ranger-regression/nanoxml_eqchk/nanoxml_prop3.yml
+#java-ranger-regression/printtokens_eqchk/printtokens_prop2.yml
+#java-ranger-regression/replace5_eqchk/replace5_prop2.yml
+#java-ranger-regression/siena_eqchk/siena_prop2.yml
+#java-ranger-regression/WBS/WBS_prop1.yml
+#java-ranger-regression/WBS/WBS_prop3.yml
+#java-ranger-regression/WBS/WBS_prop4.yml
+#
+#
